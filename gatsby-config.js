a54a583c350c8e58bae7668939b081a0cfe9fd8d
andrejtachko <atacigin@gmail.com>
2019-10-21 18:31:24 +0200

    add navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dada Apartman`,
+    description: `Apartman Dada u Medulinu.`,
+    author: `Andrej Tacigin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,44 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./layout.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+const Header = () => (
+  <header id="header">
+    <nav className="navbar navbar-expand-sm  navbar-dark bg-dark">
+      <div className="container">
+        <a href="" className="navbar-brand">
+          Dada Apartmani
+        </a>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarNav"
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link">Home</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link">Home</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
