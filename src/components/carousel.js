ee3ef023ccad4a0b6edb49503591ef214b86945b
andrejtachko <atacigin@gmail.com>
2019-10-24 23:14:32 +0200

    fix carousel full screen

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react"
 import $ from "jquery"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import car1 from "../images/car1.jpg"
+import car2 from "../images/car2.jpg"
+import car3 from "../images/car3.jpg"
 
 export default class Carousel extends Component {
   componentDidMount() {
@@ -11,64 +16,62 @@ export default class Carousel extends Component {
   }
   render() {
     return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-sm-8 m-auto">
-            <div
-              id="slider3"
-              className="carousel slide mb-5"
-              data-ride="carousel"
-              data-keyboard="true"
-            >
-              <ol className="carousel-indicators">
-                <li
-                  className="active"
-                  data-target="#slider3"
-                  data-slide-to="0"
-                ></li>
-                <li data-target="#slider3" data-slide-to="1"></li>
-                <li data-target="#slider3" data-slide-to="2"></li>
-              </ol>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img
-                    className="d-block img-fluid"
-                    src="https://source.unsplash.com/wgq4eit198Q/700x400"
-                    alt="First Slide"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    className="d-block img-fluid"
-                    src="https://source.unsplash.com/WLUHO9A_xik/700x400"
-                    alt="Second Slide"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    className="d-block img-fluid"
-                    src="https://source.unsplash.com/4yta6mU66dE/700x400"
-                    alt="Third Slide"
-                  />
-                </div>
+      <div className="row">
+        <div className="col-sm-12">
+          <div
+            id="slider3"
+            className="carousel slide"
+            data-ride="carousel"
+            data-keyboard="true"
+          >
+            <ol className="carousel-indicators">
+              <li
+                className="active"
+                data-target="#slider3"
+                data-slide-to="0"
+              ></li>
+              <li data-target="#slider3" data-slide-to="1"></li>
+              <li data-target="#slider3" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  className="d-block  min-vh-100 img-fluid"
+                  src={car1}
+                  alt="First Slide"
+                />
               </div>
-
-              <a
-                href="#slider3"
-                className="carousel-control-prev"
-                data-slide="prev"
-              >
-                <span className="carousel-control-prev-icon"></span>
-              </a>
-
-              <a
-                href="#slider3"
-                className="carousel-control-next"
-                data-slide="next"
-              >
-                <span className="carousel-control-next-icon"></span>
-              </a>
+              <div className="carousel-item">
+                <img
+                  className="d-block min-vh-100 img-fluid"
+                  src={car2}
+                  alt="Second Slide"
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block min-vh-100 img-fluid"
+                  src={car3}
+                  alt="Third Slide"
+                />
+              </div>
             </div>
+
+            <a
+              href="#slider3"
+              className="carousel-control-prev"
+              data-slide="prev"
+            >
+              <span className="carousel-control-prev-icon"></span>
+            </a>
+
+            <a
+              href="#slider3"
+              className="carousel-control-next yolo"
+              data-slide="next"
+            >
+              <span className="carousel-control-next-icon"></span>
+            </a>
           </div>
         </div>
       </div>
